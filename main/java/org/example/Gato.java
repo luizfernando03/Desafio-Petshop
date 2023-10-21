f019ef17bcf979416d8232a9a9071bead0d32405
Luiz Fernando <luizfernandodemoura.2013@gmail.com>
2023-10-20 22:45:19 -0300

    crinado herança

---
+++ main/java/org/example/Gato.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import javax.xml.crypto.Data;
+
+public class Gato extends Animal{
+    public Gato(String nome, String especie, String raca, Data dataNascimento, String proprietario) {
+        super(nome, especie, raca, dataNascimento, proprietario);
+    }
+}
